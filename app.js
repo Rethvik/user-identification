5e753f2ca45746cb6be9c49fe3ded6259edb49fa
Rethvik <84142399+Rethvik@users.noreply.github.com>
2021-10-26 22:26:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://rethvik:rethvik45@nodejs.xupzf.mongodb.net/node-auth";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(PORT))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home"));
+// app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
